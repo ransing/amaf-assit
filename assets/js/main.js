e792d9b4eba1a297513bbadd32878ae84834485d
viraj.r@area23hc.com <viraj.r@area23hc.com>
2022-06-29 21:36:49 -0700

    TASK: before the day of

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-/**
-* Template Name: Reveal - v4.7.0
-* Template URL: https://bootstrapmade.com/reveal-bootstrap-corporate-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+// import {confetti} from "./confetti.js"
+
 (function() {
   "use strict";
 
@@ -20,8 +16,87 @@
         }, false)
       })
 
+  //easter egg
+
+      let buffer = [];
+  document.addEventListener('keydown', evt => {
+    const triggerWords = ['mateo','viraj','ana-maria','davis','anan','samantha','linnea','annie','hayley','daniel','lauren','burak','diana','tanya','ammy','pete','wei','rachel','nohelia','dana']
+    const key = evt.key.toLowerCase();
+    const currentTime = Date.now();
+    let lastKeyTime = Date.now();
+    console.log('easeter')
+    
+    // if (triggerWord.indexOf(key) === -1) return;
+    if (currentTime - lastKeyTime > 1000) {
+        buffer = [];
+      }
+      buffer.push(key);
+      lastKeyTime = currentTime;
+      let typedWord = buffer.join("")
+      if (triggerWords.includes(typedWord)) {
+        // console.log("easter triggered", buffer, key)
+        animateConfetti()
+        if (document.getElementById('credits')) {
+        document.getElementById('credits').classList.add('loaded');
+        }
+          else return false;  
+        }
+  })
+
+
+  // confettii 
+  var confettiShower = [];
+  var numConfettis = 400;
+  var confettiContainer = document.getElementById('confetti-div');
+  var colors = [
+    "#00FF73  ",
+    "#6C4AE2",
+    "#FDDA00 ",
+    "#DB27DB ",
+    "#FA405A ",
+    "#51EFFC ",
+    "#EB640A "
+  ];
+  
+  class Confetti {
+    constructor(x, y, w, h, c) {
+      this.w = Math.floor(Math.random() * 15 + 5);
+      this.h = this.w*1.2;
+      this.x = Math.floor(Math.random() * 100);
+      this.y = Math.floor(Math.random() * 100);
+      this.c = colors[Math.floor(Math.random() * colors.length)];
+    }
+    create() {
+        var newConfetti = '<div class="confetti" style="bottom:' + this.y +'%; left:' + this.x +'%;width:' +
+          this.w +'px; height:' + this.h +'px;"><div class="rotate"><div class="askew" style="background-color:' + this.c + '"></div></div></div>';
+        confettiContainer.innerHTML+= newConfetti; 
+        }
+    };
+  
+  function animateConfetti() {
+    console.log('confetti triggered')
+    for (var i = 1; i <= numConfettis; i++) {
+      var confetti = new Confetti();
+      confetti.create();
+    }
+    var confettis = document.querySelectorAll('.confetti');
+    for (var i = 0; i < confettis.length; i++) {
+      var opacity = Math.random() + 0.1;
+      var animated = confettis[i].animate([
+        { transform: 'translate3d(0,0,0)', opacity: opacity },
+        { transform: 'translate3d(20vw,100vh,0)', opacity: 1 }
+      ], {
+        duration: Math.random() * 3000 + 3000,
+        iterations: Infinity,
+        delay: -(Math.random() * 5000)
+      });
+     confettiShower.push(animated);
+    }
+  }
+
+
   document.querySelector('.search-form-control').addEventListener('input',function(e){
-    console.log(e.target.value.length)
+    // console.log(e.target.value.length)
     if (e.target.value.length < 5 ){
       document.queryselector(".invalid-feedback").display = 'none'
     }
@@ -42,16 +117,34 @@
     }
     document.querySelector('.zip-code-stored').innerHTML = e.target.value
     const threatLevel = [["Mild","green"], ["Moderate", "gold"], ["High", "red"]]
+    const threatLevelSpan = [["Templado","green"], ["Moderado", "gold"], ["Aldo", "red"]]
     const airQualityIndex = [['Good',"green"],['Moderate','yellow'], ['Unhealthy for Sensitive Groups',"orange"],['Unhealthy','red'],['Very Unhealthy','maroon']]
+    const airQualityIndexSpan = [['Bueno',"green"],['Moderado','yellow'], ['Malsano para grupos sensibles',"orange"],['Malsano','red'],['Muy Insalubre','maroon']]
     const randomthreatLevel = threatLevel[Math.floor(Math.random() * threatLevel.length)];
     const randomAirLevel = airQualityIndex[Math.floor(Math.random() * airQualityIndex.length)];
-    document.querySelector('.threat-level').innerHTML = randomthreatLevel[0]
-    document.querySelector('.threat-level').style.backgroundColor = randomthreatLevel[1]
+    if (languagetoggleIsOn === false){
+      const randomthreatLevel = threatLevel[Math.floor(Math.random() * threatLevel.length)];
+      document.querySelector('.threat-level').innerHTML = randomthreatLevel[0]
+      document.querySelector('.threat-level').style.backgroundColor = randomthreatLevel[1]
+      // console.log(randomthreatLevel[0], randomthreatLevel[1])
+      const randomAirLevel = airQualityIndex[Math.floor(Math.random() * airQualityIndex.length)];
     document.querySelector('.air-quality-index').innerHTML = randomAirLevel[0]
     document.querySelector('.air-quality-index').style.backgroundColor = randomAirLevel[1]
+    }
+    else {
+      const randomthreatLevelSpan = threatLevelSpan[Math.floor(Math.random() * threatLevelSpan.length)];
+      document.querySelector('.threat-level').innerHTML = randomthreatLevelSpan[0]
+      document.querySelector('.threat-level').style.backgroundColor = randomthreatLevelSpan[1]
+      const randomAirLevelSpan = airQualityIndexSpan[Math.floor(Math.random() * airQualityIndexSpan.length)];
+    document.querySelector('.air-quality-index').innerHTML = randomAirLevelSpan[0]
+    document.querySelector('.air-quality-index').style.backgroundColor = randomAirLevelSpan[1]
+    }
+    // document.querySelector('.threat-level').innerHTML = randomthreatLevel[0]
+    // document.querySelector('.threat-level').style.backgroundColor = randomthreatLevel[1]
+    // document.querySelector('.air-quality-index').innerHTML = randomAirLevel[0]
+    // document.querySelector('.air-quality-index').style.backgroundColor = randomAirLevel[1]
     document.querySelector('.population').innerHTML = Math.floor(Math.random() * (94000 - 50000) + 50000);
     
-  
   })
 
   document.querySelector(".fill-out-form-btn").addEventListener("click", () => {
@@ -61,6 +154,89 @@
   document.querySelector(".search-zip").addEventListener("click", () => {
     window.location.href = "/#hero"
   })
+
+  let toggle = document.getElementById('lang-toggle');
+  var languagetoggleIsOn = false;
+  toggle.addEventListener('click', e => {
+    // currentLanguage.innerHTML = languages[+!toggle.checked ? 0 : 1];
+    // console.log("lang toggle", toggle.checked)
+    
+    if (languagetoggleIsOn === false) {
+      // languageSwitch.classList.add('toggle-is-active');
+      languagetoggleIsOn = true;
+
+      document.getElementById("hero-png").src = "./assets/img/breathe-easy-icon-es.png"
+      document.querySelector(".btn-get-started").textContent = "Empezar"
+      document.querySelector(".btn-projects").textContent = "Socios"
+      document.querySelector("#search-form h2").textContent = "¿Cuál es su código postal del Bronx (10451-10475)?"
+      document.querySelector("#check-1").textContent = "Niño con asma en Escuela Pública"
+      document.querySelector("#check-2").textContent = "AMAF en el registro"
+      document.querySelector("#name").placeholder = "código postal";
+      document.querySelector(".search-zip").textContent = "Buscar";
+      document.querySelector(".invalid-feedback").textContent = "Proporcione un código postal válido del Bronx (10451-10475)."
+      document.querySelector("#zipcode-title").textContent = "Código postal";
+      document.querySelector("#population-title").textContent = "Población";
+      document.querySelector("#asthma-risk").textContent = "Riesgo de asma";
+      document.querySelector("#air-quality").textContent = "Índice de calidad del aire";
+      document.querySelector(".mock-data").textContent = "datos simulados";
+      document.querySelector(".fill-out-form-btn").textContent = "Rellenar formulario AMAF";
+      document.querySelector("#modal-close").textContent = "Cerrar";
+      document.querySelector("#what-is").innerHTML = "¿Qué es&nbsp;";
+      document.querySelector("#amaf-h3").textContent = "Formulario de administración de medicamentos para el asma";
+      document.querySelector("#list-1").textContent = "Formulario de administración de medicamentos para el asma";
+      document.querySelector("#list-2").textContent = "Las escuelas de la Ciudad de Nueva York (NYC) requieren que el cuidador presente un Formulario de Administración de Medicamentos para el Asma (AMAF; un formulario de pedido completado por un médico que debe renovarse cada año escolar) para confirmar los diagnósticos de asma y permitir que las enfermeras escolares administren medicamentos para las exacerbaciones del asma.";
+      document.querySelector("#list-3").textContent = "Sin un AMAF, debido a las regulaciones basadas en sistemas, la enfermera no puede administrar medicamentos, incluso si un estudiante tiene un ataque de asma.";
+      document.querySelector("#download-form").textContent = "Descargar formulario aquí"
+      document.querySelector("#contact h2").textContent = "Boletín y recordatorios"
+      document.querySelector("#contact p").textContent = "Regístrese para recibir información actualizada sobre la salud respiratoria en el Bronx. Configure recordatorios para el registro y las renovaciones anuales de AMAF. Los recordatorios se enviarán 1 mes antes de la fecha de vencimiento del formulario."
+      document.querySelector("#newletter-label").textContent = "Boletin informativo"
+      document.querySelector("#reminder-label").textContent = "Recordatorio AMAF"
+      document.querySelector("#sign-up-btn").textContent = "Inscribirse"
+      document.querySelector("#clients h2").textContent = "Socios"
+      document.querySelector("#sign-up-disclaimer").textContent = "Característica próximamente."
+      document.querySelector("#social-media").textContent = "Contacta con nosotros"
+      
+    } 
+  else  {
+      // languageSwitch.classList.remove('toggle-is-active');
+      languagetoggleIsOn = false;
+
+      document.getElementById("hero-png").src = "./assets/img/breathe-easy-icon.png"
+      document.querySelector(".btn-get-started").textContent = "Get Started"
+      document.querySelector(".btn-projects").textContent = "Partners"
+      document.querySelector("#search-form h2").textContent = "What is your Bronx zip code (10451-10475)?"
+      document.querySelector("#check-1").textContent = "Child with asthma in Public School"
+      document.querySelector("#check-2").textContent = "AMAF on record"
+      document.querySelector("#name").placeholder = "Zip code";
+      document.querySelector(".search-zip").textContent = "Search";
+      document.querySelector(".invalid-feedback").textContent = "Please provide a valid Bronx zip code (10451-10475)."
+      document.querySelector("#zipcode-title").textContent = "Zip Code";
+      document.querySelector("#population-title").textContent = "Population";
+      document.querySelector("#asthma-risk").textContent = "Asthma Risk";
+      document.querySelector("#air-quality").textContent = "Air Quality Index";
+      document.querySelector(".mock-data").textContent = "* mock data";
+      document.querySelector(".fill-out-form-btn").textContent = "Fill out AMAF form";
+      document.querySelector("#modal-close").textContent = "Close";
+      document.querySelector("#what-is").innerHTML = "What is&nbsp;";
+      document.querySelector("#amaf-h3").textContent = "Asthma Medication Administration Form";
+      document.querySelector("#list-1").textContent = "Successful school asthma management involves individual case identification and provision of medications to students when needed.";
+      document.querySelector("#list-2").textContent = "New York City (NYC) schools require caregiver submission of an Asthma Medication Administration Form (AMAF; a physician-completed order form that must be renewed every school year) to confirm asthma diagnoses and allow school nurses to administer medication for asthma exacerbations.";
+      document.querySelector("#list-3").textContent = "Without an AMAF, due to systems-based regulations, the nurse cannot administer medication, even if a student is having an asthma attack.";
+      document.querySelector("#download-form").textContent = "Download Form here "
+      document.querySelector("#contact h2").textContent = "Newsletter & Reminders"
+      document.querySelector("#contact p").textContent = "Sign up for up to date information about respiratory health in Bronx. Set up reminders for annual AMAF registration and renewals. Reminders will be sent 1 month before the form is due."
+      document.querySelector("#newletter-label").textContent = "Newsletter"
+      document.querySelector("#reminder-label").textContent = "AMAF reminder"
+      document.querySelector("#sign-up-btn").textContent = "Sign Up"
+      document.querySelector("#clients h2").textContent = "Partners"
+      document.querySelector("#sign-up-disclaimer").textContent = "Feature coming soon."
+      document.querySelector("#social-media").textContent = "Contact Us"
+      
+    }
+    
+    return languagetoggleIsOn;
+   
+  });
 
   // const checkFormValid = () => {
   //   console.log('onkeychange')
